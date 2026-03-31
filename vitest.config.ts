f95f1c0935e5f21d69d0603a211e808c9bcfedc4
UnABC <xiaotudou888@gmail.com>
2026-03-31 13:45:09 +0900

    テスト項目にcoverageを追加

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,5 +8,16 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['src/setup-vitest.js'],
     include: ['src/app/**/**/*.spec.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'], // text: コンソール出力用, html/json: CIのアーティファクト用
+      // CIで一定のカバレッジを下回った場合にジョブを失敗させる（Fail-fast）設定
+      thresholds: {
+        lines: 80,
+        functions: 80,
+        branches: 80,
+        statements: 80,
+      },
+    },
   },
 });

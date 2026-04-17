d1e009216d2f5b4cd433aca4b4e064ad535893d0
UnABC <xiaotudou888@gmail,com>
2026-04-17 15:00:11 +0900

    認証手前まで

---
+++ src/app/features/mypage/mypage.component.ts
@@ -0,0 +1,8 @@
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-mypage',
+  templateUrl: './mypage.component.html',
+  styleUrl: './mypage.component.css',
+})
+export class MypageComponent {}

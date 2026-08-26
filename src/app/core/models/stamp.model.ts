d87f3edc9bef9934e2c9a55d24e60293e6faffff
UnABC <xiaotudou888@gmail,com>
2026-08-26 20:31:46 +0900

    スタンプ一覧の表示の高速化

--- src/app/core/models/stamp.model.ts
+++ src/app/core/models/stamp.model.ts
@@ -1,12 +1,6 @@
 export interface Stamp {
   id: string;
   name: string;
-  creatorId?: string;
-  createdAt?: string;
-  updatedAt?: string;
-  fileId?: string;
-  isUnicode?: boolean;
-  hasThumbnail?: boolean;
 }
 
 //GIF用
